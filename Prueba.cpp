6d2459e9ee1d006721565ecc4f57c19327bff981
Hector Guerrero Diaz <hjesus.guerrero@alumnos.udg.mx>
2020-09-17 13:17:25 -0500

    Capturamos la edad y sumamos 1

--- Prueba.cpp
+++ Prueba.cpp
@@ -3,10 +3,15 @@
 int main()
 {
     char nombre[200];
+    unsigned int edad;
+
     printf("hola mundo\n");
     printf("Nombre:\n");
     fgets(nombre, sizeof(nombre), stdin);
     printf("Hola %s",nombre);
+    printf("Edad:\n");
+    scanf("%u",&edad);
+    printf("Vas a cumplir: %u", edad + 1);
 
     return 0;
 }
